77b998a35db5906b86b1d8a792223ee48891a3ae
oouuyab <oouuyab@gmail.com>
2022-12-13 22:25:40 +0900

    feat: [Util] Axios error handler 추가

--- src/util/Util.js
+++ src/util/Util.js
@@ -1,2 +1,4 @@
 export const convertInternationalPhoneNumber = (hpNo) =>
-  '+82-' + hpNo.slice(1, 3) + '-' + hpNo.slice(3, 7) + '-' + hpNo.slice(7);
+	'+82-' + hpNo.slice(1, 3) + '-' + hpNo.slice(3, 7) + '-' + hpNo.slice(7);
+
+export const axiosErrorHandler = (error) => alert(error.response.data.message);
